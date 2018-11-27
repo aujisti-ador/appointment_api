ebfadda534909ea29f35d0a2d04e011496cc5d64
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2018-11-27 18:06:01 +0600

    passport/login authentication done

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -21,13 +21,12 @@ class UserController extends Controller
     {
         if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
             $user = Auth::user();
-            $success['token'] = $user->createToken('MyApp')->accessToken;
-            return response()->json(['success' => $success], $this->successStatus);
+            $success['token'] = $user->createToken('appointment_api')->accessToken;
+            return response()->json(['success' => $success, 'user' => $user], $this->successStatus);
 
         } else {
             return response()->json(['error' => 'Unauthorised'], 401);
         }
-
     }
 
     /**
@@ -35,33 +34,76 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+//    public function register(Request $request)
+//    {
+//        /**
+//         * Register api
+//         *
+//         * @return \Illuminate\Http\Response
+//         */
+//        $validator = Validator::make($request->all(), [
+//            'name' => 'required',
+//            'email' => 'required|email',
+//            'password' => 'required',
+//            'c_password' => 'required|same:password',
+//        ]);
+//
+//        if ($validator->fails()) {
+//            return response()->json(['error' => $validator->errors()], 401);
+//        }
+//
+//        $input = $request->all();
+//
+//        $input['password'] = bcrypt($input['password']);
+//        $user = User::create($input);
+//        $success['token'] = $user->createToken('MyApp')->accessToken;
+//        $success['name'] = $user->name;
+//        return response()->json(['success' => $success], $this->successStatus);
+//
+//    }
+
+
     public function register(Request $request)
+
     {
-        /**
-         * Register api
-         *
-         * @return \Illuminate\Http\Response
-         */
+
         $validator = Validator::make($request->all(), [
+
             'name' => 'required',
+
             'email' => 'required|email',
+
             'password' => 'required',
+
             'c_password' => 'required|same:password',
+
         ]);
 
+
         if ($validator->fails()) {
+
             return response()->json(['error' => $validator->errors()], 401);
+
         }
+
 
         $input = $request->all();
 
         $input['password'] = bcrypt($input['password']);
+
         $user = User::create($input);
-        $success['token'] = $user->createToken('MyApp')->accessToken;
+
+        $success['token'] = $user->createToken('appointment_api')->accessToken;
+
         $success['name'] = $user->name;
+
+        $success['email'] = $user->email;
+
+
         return response()->json(['success' => $success], $this->successStatus);
 
     }
+
 
     /**
      * Store a newly created resource in storage.
@@ -74,6 +116,7 @@ class UserController extends Controller
         $user = Auth::user();
         return response()->json(['success' => $user], $this->successStatus);
     }
+
 
     /**
      * Display the specified resource.
