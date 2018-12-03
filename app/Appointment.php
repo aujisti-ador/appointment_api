e61e83d6e3c4d051cf4831773c3bdbd87ceff62c
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2018-12-03 19:07:27 +0600

    appointment table model updated

--- app/Appointment.php
+++ app/Appointment.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    protected $fillable = ['host_id', 'guest_id', 'note', 'location', 'appointment_status_id', 'assistant_id'];
-    protected $visible = [];
+    protected $fillable = ['host_id', 'guest_id', 'note', 'location', 'appointment_status_id', 'assistant_id', 'avatar'];
+    protected $visible = ['host_id', 'guest_id', 'note', 'location', 'appointment_status_id', 'assistant_id', 'avatar'];
 }
