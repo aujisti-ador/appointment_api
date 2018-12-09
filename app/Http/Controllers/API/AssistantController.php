4c1d0c382edb2b80f32eea8297145e3c5fb82d16
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2018-12-09 18:09:18 +0600

    assistant role to show appointment added

--- app/Http/Controllers/API/AssistantController.php
+++ app/Http/Controllers/API/AssistantController.php
@@ -7,6 +7,7 @@ use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
 class AssistantController extends Controller
@@ -36,21 +37,21 @@ class AssistantController extends Controller
         $org_id_ast = $user->organizations_id;
 
 //        dd($appointments->host_id);
-//        dd($ast_id);
+//        dd($user->id);
 
         foreach ($appointments as $appointment) {
 //            dd($appointment->host_id);
+            $is_ast_role = DB::table('user_roles')->where([['user_id', $user->id], ['role_id', 5]])->first();
+//            dd($is_ast_role);
             $usr = User::find($appointment->host_id);
-            if ($usr->organizations_id == $org_id_ast) {
+            if ($usr->organizations_id == $org_id_ast && !empty($is_ast_role)) {
                 $res = Appointment::where([['date', Carbon::today()], ['appointment_status_id', 1]])->get();
                 return response()->json(['success' => $res]);
             } else {
-                return response()->json(['success' => "no data!"]);
+                return response()->json(['success' => "no data or not authorized!"]);
             }
 
         }
-
-
 
 
 //        if ($user->organizations_id == $result-> organizations_id) {
